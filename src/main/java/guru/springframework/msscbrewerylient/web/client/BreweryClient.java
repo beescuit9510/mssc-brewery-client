04a570c47aaad266917f7b2bc59943ac5df5a9f9
beescuit9510 <pengsoon20@gmail.com>
2022-03-14 09:49:45 +0900

    HTTP GET with Spring RestTemplate

---
+++ src/main/java/guru/springframework/msscbrewerylient/web/client/BreweryClient.java
@@ -0,0 +1,36 @@
+package guru.springframework.msscbrewerylient.web.client;
+
+import guru.springframework.msscbrewerylient.web.model.BeerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID ;
+
+@Component
+@ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
+public class BreweryClient {
+
+    private final String BEER_PATH_V1 = "/api/v1/beer/";
+    private String apihost;
+
+    private final RestTemplate restTemplate;
+
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeerById(UUID uuid){
+
+        String API_URL_OR_PATH = apihost+BEER_PATH_V1+uuid.toString();
+
+        Class<BeerDto> expectedReturnObjClass = BeerDto.class;
+
+        return restTemplate.getForObject(API_URL_OR_PATH, expectedReturnObjClass);
+    }
+
+    public void setApihost(String apihost) {
+        this.apihost = apihost;
+    }
+}
---
+++ src/test/java/guru/springframework/msscbrewerylient/MsscBreweryLientApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbrewerylient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryLientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
